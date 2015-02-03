a07614b36402a393411d00cb7b5e7a3a673371f8
Christopher Wu <chriswu452@gmail.com>
2015-02-03 10:00:37 -0800

    Added change task custom actions

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :set_task, only: [:show, :edit, :update, :destroy, :change]
 
   # GET /tasks
   # GET /tasks.json
@@ -64,6 +64,12 @@ class TasksController < ApplicationController
     end
   end
 
+  def change
+    @task.update_attributes(state: params[:state])
+    respond_to do |format|
+      format.html {redirect_to tasks_path, notice: "Task Updated"}
+    end
+  end
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_task
